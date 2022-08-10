4e861ee5cc13ecb4fc7dbd44e2d75f5174264a4f
Jarosław Olszewski <oljar@tlen.pl>
2022-08-10 19:22:00 +0200

    Metody i klasy STATYCZNE

---
+++ ABC.BL/Klasy i Metody Statyczne.md
@@ -0,0 +1,46 @@
+﻿***Metody statyczne C# - przykład ***
+
+{
+    Arytmetyka art = new Arytmetyka();
+    int wynik = art.Dodawanie(1, 2);
+}
+Aby zadeklarować ,że dana zmienna bądź metoda jest statyczna musimy dopisać słowo kluczowe static po modyfikatorze dostępu ,ale przed określeniem typu.
+
+
+class Arytmetyka 
+{
+    public static int Dodawanie(int a, int b)
+    {
+        return a + b;
+    }
+}  
+
+Teraz możesz mieć dostęp do tej metody poprzez odwołanie się do samej nazwy klasy. Jak to widać w kodzie.
+{
+    int wynik = Arytmetyka.Dodawanie(1, 10);
+}
+
+Kiedy definiujesz statyczną metodę nie może ona korzystać z żadnych pól zdefiniowanych w klasie. Chyba że pola te są też statyczne. Czyli statyczne metody mogą odwoływać się tylko do innych statycznych metod i pól. Niestatyczne pola i metody wymagają utworzenia obiektu, do których będą one odwoływane.
+
+***Klasy statyczne C#***
+
+{
+    public  static class Drukuj
+    {
+        public static void Wydruk()
+        {
+            Console.WriteLine("ala ma kota");
+        }
+        
+    }
+}
+
+
+W klasie statycznej mogą znajdować się tylko pola statyczne. Aby stworzyć klasę statyczną wystarczy dopisać słowo static przed słowem class.
+
+Wywołanie
+
+{
+    Drukuj.Wydruk();
+
+}
---
+++ commonTest/UslugaLogowania.cs
@@ -0,0 +1,57 @@
+﻿using ABC.BL;
+using Common;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace commonTest
+{
+    [TestClass]
+    public class UslugaLogowania
+    {
+        [TestMethod]
+        public void PiszDoPlikuTest()
+        {
+            //Arrange
+
+            var zmienioneElementy = new List<ILogowanie>();
+            aaa.azor();
+         
+
+            var klent = new Klient(1)
+            {
+                Email = "admin@dev-hobby.pl",
+                Imie = "Mariusz",
+                Nazwisko = "Kowal",
+                ListaAdresow = null
+
+            };
+
+            zmienioneElementy.Add(klent as ILogowanie);
+
+            var produkt = new Produkt(2)
+            {
+                NazwaProduktu = " Klocki",
+                Opis = " KLocki do zabawy",
+                AktualnaCena = 120M
+
+
+            };
+            zmienioneElementy.Add(produkt as ILogowanie);
+
+
+            var zamowienie = new Zamowienie(3)
+            {
+                DataZamowienia = new DateTime(2018, 9, 21)
+            };
+            zmienioneElementy.Add(zamowienie as ILogowanie);
+
+
+            //Act
+            UslugaLogowanie.PiszDoPliku(zmienioneElementy);
+            // Wywołanie metody statycznej C#  
+            
+        
+        }
+    }
+}
